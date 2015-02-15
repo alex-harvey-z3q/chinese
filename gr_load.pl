8b3886a7f8de40034fb92d1fc0b754cc0e5ac538
Alex Harvey <Alex.Harvey@team.telstra.com>
2015-02-15 22:01:18 +1100

    Allow 。in gr_load.pl

--- gr_load.pl
+++ gr_load.pl
@@ -35,6 +35,10 @@ for (;;) {
             push @trad, '？';
             push @piny_c, '?';
             next;
+        } elsif ($chars[$c] eq '。') {
+            push @trad, '。';
+            push @piny_c, '.';
+            next;
         } elsif ($chars[$c] eq '，') {
             push @trad, '，';
             push @piny_c, ',';
